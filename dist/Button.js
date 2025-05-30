5f7bc0f4374e37383e4977fdd55889efebe81cfc
Homayoun <homayoun763@gmail.com>
2025-05-30 20:27:41 +0330

    add : react

--- dist/Button.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import './button.css';
-export const Button = ({ children }) => {
-    return _jsx("button", { className: "primary-btn", children: children });
-};
--- dist/types/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './button.css';
-interface ButtonProps {
-    childern: React.ReactNode;
-}
-export declare const Button: React.FC<ButtonProps>;
-export {};
